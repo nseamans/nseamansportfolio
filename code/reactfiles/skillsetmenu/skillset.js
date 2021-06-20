f08a2664b8b70ea3381338e672f0ad78a68c9929
Nicholas Seamans <nseamans@radford.edu>
2021-06-19 22:28:36 -0700

    Added Education Section
    Added Section pertaining to my education.

--- code/reactfiles/skillsetmenu/skillset.js
+++ code/reactfiles/skillsetmenu/skillset.js
@@ -48,8 +48,79 @@ class Skillset extends React.Component {
         <br />
         {menuinfo}
         <br />
-        <h4 class='menu_head'>Education and Hobbies</h4>
+        <h4 class='menu_head'>Education</h4>
+        
         <hr />
+
+        <div class="education__container">
+            <div class="education__wrapper">
+              <h6 class="education__textright">
+                Radford University
+              </h6>
+            </div>
+            <div class="copy-container">
+              <div class="copy-wrapper">
+                <div class="row">
+                  <p>
+                    Degree: Bachelors of Science
+                  </p>
+                </div>
+                <div class="row">
+                  <p>
+                    Major: Information Science (Web Development Concentration)
+                  </p>
+                </div>
+                <div class="row">
+                  <p>
+                    Coursework: Computer Science 1/2/3, Software Engineering,
+                    Web Development 1/2/3, Content Management Systems 1/2, 
+                    Software Testing, Database (Programming) 1/2, 
+                    GUI Programming 1, Web Graphics, Web Animation
+                  </p>
+                </div>
+                <div class="row">
+                  <p>
+                    Organizations: Upsilon Pi Epsilon (Computer Science Honors Society)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="education__container">
+            <div class="education__wrapper">
+              <h6 class="education__textright">
+                Virginia Western <br/> Community College
+              </h6>
+            </div>
+            <div class="copy-container">
+              <div class="copy-wrapper">
+                <div class="row">
+                  <p>
+                    Degree: Associates of Applied Science
+                  </p>
+                </div>
+                <div class="row">
+                  <p>
+                    Major: Information Systems (Network and Database Concentration)
+                  </p>
+                </div>
+                <div class="row">
+                  <p>
+                    Coursework: Network Administration 1/2, Database (Management) 1/2, 
+                    Web Programming 1, Programming 1, MS OS and Server Administration 1/2
+                  </p>
+                </div>
+                <div class="row">
+                  <p>
+                    Organizations: Phi Theta Kappa (Associates Level Honors Society)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        
       </div>
     );
   }
